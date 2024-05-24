3a50803c82b87f89ffc8cf6ab0a1f721a364c87f
Luanleandro <luan.barbosacruz@gmail.com>
2024-05-24 11:34:30 -0300

    Exercicio 5: Valores Booleanos

---
+++ exercicio-tipos/src/ValoresBooleanos.java
@@ -0,0 +1,11 @@
+public class ValoresBooleanos {
+    public static void main(String[] args) {
+        boolean verdadeiro = true;
+        boolean falso = false;
+
+        // Operações lógicas
+        System.out.println("Operação lógica E: " + (verdadeiro && falso));
+        System.out.println("Operação lógica OU: " + (verdadeiro || falso));
+        System.out.println("Operação lógica NÃO: " + (!verdadeiro));
+    }
+}
