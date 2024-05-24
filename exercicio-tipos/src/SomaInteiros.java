0bc547628b7d039ed51a24fd56d02c1f6f2beec3
Luanleandro <luan.barbosacruz@gmail.com>
2024-05-24 11:32:03 -0300

    Exercicio 1: Soma de dois valores inteiros.

---
+++ exercicio-tipos/src/SomaInteiros.java
@@ -0,0 +1,8 @@
+public class SomaInteiros {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int soma = num1 + num2;
+        System.out.println("A soma de " + num1 + " e " + num2 + " é igual a " + soma);
+    }
+}
