e6abfe9f3975e9666d947f6b72a7cb2a50188356
Luanleandro <luan.barbosacruz@gmail.com>
2024-05-24 11:32:58 -0300

    Exercicio 2: Conversão de tipos.

---
+++ exercicio-tipos/src/ConversaoTipos.java
@@ -0,0 +1,9 @@
+public class ConversaoTipos {
+  public static void main(String[] args) {
+      double numeroDouble = 10.5;
+      int numeroInteiro = (int) numeroDouble;
+      
+      System.out.println(numeroDouble);
+      System.out.println(numeroInteiro);
+  }
+}
