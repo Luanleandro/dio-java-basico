459751dca4877fc72acaf9522edef7a2713402f2
Luanleandro <luan.barbosacruz@gmail.com>
2024-05-27 10:54:41 -0300

    Aula Operadores-3

--- tipos-variaveis/src/OperadoresJava.java
+++ tipos-variaveis/src/OperadoresJava.java
@@ -1,31 +1,43 @@
 public class OperadoresJava {
   public static void main(String[] args) {
-    String nomeCompleto = "LINGUAGEM" + "JAVA";
+    // String nomeCompleto = "LINGUAGEM" + "JAVA";
 
-    System.out.println(nomeCompleto);
-    String concatenacao = "?";
+    // System.out.println(nomeCompleto);
+    // String concatenacao = "?";
     
-    System.out.println(concatenacao);
+    // System.out.println(concatenacao);
 
-    concatenacao = 1 + 1 + 1 + "1";
+    // concatenacao = 1 + 1 + 1 + "1";
 
-    System.out.println(concatenacao);
+    // System.out.println(concatenacao);
 
-    concatenacao = 1 + "1" + 1 + 1;
+    // concatenacao = 1 + "1" + 1 + 1;
 
-    System.out.println(concatenacao);
+    // System.out.println(concatenacao);
 
-    concatenacao = 1 + "1" + 1 + "1";
+    // concatenacao = 1 + "1" + 1 + "1";
 
-    System.out.println(concatenacao);
+    // System.out.println(concatenacao);
 
-    concatenacao = "1" + 1 + 1 + 1;
+    // concatenacao = "1" + 1 + 1 + 1;
 
-    System.out.println(concatenacao);
+    // System.out.println(concatenacao);
 
-    concatenacao = "1" + (1 + 1 + 1);
+    // concatenacao = "1" + (1 + 1 + 1);
 
-    System.out.println(concatenacao);
+    // System.out.println(concatenacao);
+    
+    //Unarios
+    int numero = 5;
+    System.out.println(-numero); // -5
 
+    numero = -numero; // positivo para negativo
+
+    numero++;
+    System.out.println(numero); // -4 
+
+    numero = numero * -1; // negativo para positivo
+    
+    System.out.println(numero); // 4
   }
 }
